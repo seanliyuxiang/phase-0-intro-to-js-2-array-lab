96efbe70e5d1562b5340a8cc4b4b75032963d011
seanliyuxiang <seanliyuxiang@gmail.com>
2021-08-10 17:52:54 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -2,4 +2,38 @@
 
 const cats = ["Milo", "Otis", "Garfield"];
 
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
 
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  const catsCopy = cats.slice();
+  catsCopy.push(name);
+  return catsCopy;
+}
+
+function prependCat(name) {
+  const catsCopy = cats.slice();
+  catsCopy.unshift(name);
+  return catsCopy;
+}
+
+function removeLastCat() {
+  return [...cats.slice(0, cats.length-1)];
+}
+
+function removeFirstCat() {
+  return [...cats.slice(1)];
+}
